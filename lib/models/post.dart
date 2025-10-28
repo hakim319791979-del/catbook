d8b44a2c59d4187c41c67be31bdc38943a7e6717
hakim319791979-del <hakim319791979@gmail.com>
2025-10-28 05:10:57 +0300

    Update post.dart

--- lib/models/post.dart
+++ lib/models/post.dart
@@ -1,15 +1,41 @@
+import 'dart:convert';
+
 class Post {
   final String id;
-  final String catId;
+  final String name;
   final String caption;
+  final String? imagePath;
   final DateTime createdAt;
-  final String? imageUrl;
 
   Post({
     required this.id,
-    required this.catId,
+    required this.name,
     required this.caption,
+    this.imagePath,
     required this.createdAt,
-    this.imageUrl,
   });
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'caption': caption,
+        'imagePath': imagePath,
+        'createdAt': createdAt.toIso8601String(),
+      };
+
+  factory Post.fromJson(Map<String, dynamic> json) => Post(
+        id: json['id'],
+        name: json['name'],
+        caption: json['caption'],
+        imagePath: json['imagePath'],
+        createdAt: DateTime.parse(json['createdAt']),
+      );
+
+  static String encodeList(List<Post> posts) =>
+      jsonEncode(posts.map((p) => p.toJson()).toList());
+
+  static List<Post> decodeList(String jsonStr) =>
+      (jsonDecode(jsonStr) as List)
+          .map((data) => Post.fromJson(Map<String, dynamic>.from(data)))
+          .toList();
 }
